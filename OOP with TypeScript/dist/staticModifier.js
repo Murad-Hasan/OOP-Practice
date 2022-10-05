33d1d764acd2e659d612bbf58bc97dab399dcf61
Murad-Hasan <muradhasan.7845@gmail.com>
2022-10-06 01:10:27 +0600

    static modifier

---
+++ OOP with TypeScript/dist/staticModifier.js
@@ -0,0 +1,20 @@
+"use strict";
+class Ride {
+    start() {
+        Ride._activeRides++;
+    }
+    stop() {
+        Ride._activeRides--;
+    }
+    static get activeRides() {
+        return Ride._activeRides;
+    }
+}
+Ride._activeRides = 0;
+console.log(Ride.activeRides);
+const ride1 = new Ride();
+ride1.start();
+const ride2 = new Ride();
+ride2.start();
+console.log(Ride.activeRides);
+//# sourceMappingURL=staticModifier.js.map
---
+++ OOP with TypeScript/src/staticModifier.ts
@@ -0,0 +1,33 @@
+class Ride {
+  private static _activeRides: number = 0;
+
+  /*
+   * static property's belongs to the class itself, not to an object.
+   * it's can be accessed by the class name.
+   */
+
+  start() {
+    Ride._activeRides++;
+  }
+  stop() {
+    Ride._activeRides--;
+  }
+
+  static get activeRides() {
+    return Ride._activeRides;
+  }
+}
+
+/*
+ * static method can be called without creating an instance of the class
+ */
+console.log(Ride.activeRides); // 0
+
+const ride1 = new Ride();
+ride1.start();
+const ride2 = new Ride();
+ride2.start();
+/* 
+? ride2.stop(); 
+*/
+console.log(Ride.activeRides); // 0

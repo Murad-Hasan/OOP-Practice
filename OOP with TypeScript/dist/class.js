11a269c28dc21a5caf271e3e0962077e8dcf38f1
Murad-Hasan <muradhasan.7845@gmail.com>
2022-10-05 23:50:21 +0600

    private variable & method

--- OOP with TypeScript/dist/class.js
+++ OOP with TypeScript/dist/class.js
@@ -17,4 +17,30 @@ console.log(typeof account);
 account.deposit(500);
 console.log(account instanceof Account);
 console.log(account.balance);
+class Account1 {
+    constructor(id, owner, _balance, nickname = "default") {
+        this.id = id;
+        this.owner = owner;
+        this._balance = _balance;
+        this.nickname = nickname;
+    }
+    deposit(amount) {
+        if (amount <= 0) {
+            throw new Error("Amount must be greater than zero");
+        }
+        this._balance += amount;
+    }
+    _calculateTax() {
+        return this._balance * 0.9;
+    }
+    getTax() {
+        return this._calculateTax();
+    }
+    getBalance() {
+        return this._balance;
+    }
+}
+let account1 = new Account1(1, "John", 0, "MURAD");
+console.log(account1.nickname);
+console.log(account1.getTax());
 //# sourceMappingURL=class.js.map
--- OOP with TypeScript/src/class.ts
+++ OOP with TypeScript/src/class.ts
@@ -1,4 +1,4 @@
-//create a simple class
+//*create a simple class
 class Account {
   id: number;
   owner: string;
@@ -18,43 +18,68 @@ class Account {
   }
 }
 
-//create an instance of the class
+//*create an instance of the class
 
 let account = new Account(1, "John", 0);
-//check type of account
+//?check type of account
 console.log(typeof account);
 
 /*
-    if (typeof something === 'number' | 'object' {some logic})
-    we can use logic here
-*/
+ *if (typeof something === 'number' | 'object' {some logic})
+ *we can use logic here
+ */
 
 account.deposit(500);
-//account is instanceof Account class or not
+//?account is instanceof Account class or not
 console.log(account instanceof Account);
-//check the total balance affected by deposit
+//?check the total balance affected by deposit
 console.log(account.balance);
 
-/* ReadOnly and Optional Property  */
+/*
+ * ReadOnly and Optional Property
+ */
 
 class Account1 {
-    readonly id: number; //can't access from outside the class
-    owner: string;
-    balance: number;
-    nickname?: string; //optional property
+  readonly id: number; //*can't access from outside the class
+  owner: string;
+  private _balance: number;
+  nickname?: string; //*optional property
 
-    constructor(id: number, owner: string, balance: number) {
-        this.id = id;
-        this.owner = owner;
-        this.balance = balance;
-    }
+  constructor(
+    id: number,
+    owner: string,
+    _balance: number,
+    nickname: string = "default"
+  ) {
+    this.id = id;
+    this.owner = owner;
+    this._balance = _balance;
+    this.nickname = nickname;
+  }
 
-    deposit(amount: number): void {
-        if (amount <= 0) {
-            throw new Error("Amount must be greater than zero");
-        }
-        this.balance += amount;
+  deposit(amount: number): void {
+    if (amount <= 0) {
+      throw new Error("Amount must be greater than zero");
     }
+    this._balance += amount;
+  }
+  /*
+   * Private Method
+   */
+  private _calculateTax(): number {
+    return (this._balance * 0.9) / 100;
+  }
+  /*
+   * Public Method
+   */
+  getTax(): number {
+    return this._calculateTax();
+  }
+  getBalance(): number {
+    return this._balance;
+  }
 }
 
-
+let account1 = new Account1(1, "John", 200, "MURAD");
+console.log(account1.nickname);
+console.log(Math.round(account1.getTax()));
